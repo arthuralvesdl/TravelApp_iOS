b810693275580399e7652df137ee53e780e73637
arthuralvesdl <arthuralveslacerda@gmail.com>
2024-06-19 11:31:56 -0300

    create project

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Tellus Company on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
