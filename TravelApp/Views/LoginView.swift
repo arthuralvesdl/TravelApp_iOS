e0f5074e39970030f109f1795dbcefe80561249c
arthuralvesdl <arthuralveslacerda@gmail.com>
2024-06-20 15:39:55 -0300

    styles: button and texfield

--- TravelApp/Views/LoginView.swift
+++ TravelApp/Views/LoginView.swift
@@ -19,40 +19,42 @@ struct LoginView: View {
                 VStack (){
                     VStack(alignment: .center) {
                         Text("Travel")
-                            .font(.title)
-                            .foregroundStyle(.white)
+                            .font(.system(size: 40))
+                            .foregroundStyle(Color.white)
                         Text("Seu app de viagens")
                             .multilineTextAlignment(.center)
                             .foregroundStyle(.white)
                     }
                     .padding(.top, 15)
                     .frame(maxWidth: .infinity)
+                    
                     Spacer()
                     
                     LoginInputsView(user: $user, password: $password)
                     
-                    HStack (spacing: 10) {
-                        Button(action: {
-                            shouldRegister = true
-                        }) {
-                            Text("Registrar")
-                                .frame(width: 145, height: 30)
-                                .background(Color.white).ignoresSafeArea()
-                                .foregroundColor(.black)
-                                .cornerRadius(5)
-                        }
+                    VStack (spacing: 10) {
                         
                         Button(action: {
                             //validacao de login
                             shouldLogin = true
                         }) {
                             Text("Entrar")
-                                .frame(width: 145, height: 30)
-                                .background(Color.white).ignoresSafeArea()
-                                .foregroundColor(.black)
-                                .cornerRadius(5)
+                                .frame(width: 145, height: 40)
+                                .foregroundColor(Color(CGColor(red: 58 / 255, green: 166 / 255, blue: 200 / 255, alpha: 1))).ignoresSafeArea()
+                                .background(Color.white)
+                                .cornerRadius(10)
                         }
-                 
+                        
+                        
+                        Button(action: {
+                            shouldRegister = true
+                        }) {
+                            Text("Registrar")
+                                .frame(width: 145, height: 40)
+                                .foregroundColor(Color(CGColor(red: 58 / 255, green: 166 / 255, blue: 200 / 255, alpha: 1))).ignoresSafeArea()
+                                .background(Color.white)
+                                .cornerRadius(10)
+                        }
                     }.padding(.top, 10)
                     Spacer()
                 }
@@ -74,23 +76,23 @@ struct LoginInputsView: View {
     @Binding var password: String
     var body: some View {
         VStack () {
-            TextField("Usuário", text: $user)
-                .foregroundStyle(.black)
-                .padding(.top, 5)
-                .padding(.bottom, 5)
+            TextField("", text: $user, prompt: Text("Usuário").foregroundColor(Color(CGColor(red: 58 / 255, green: 166 / 255, blue: 200 / 255, alpha: 1))))
+                .foregroundStyle(Color(CGColor(red: 58 / 255, green: 166 / 255, blue: 200 / 255, alpha: 1)))
+                .padding(.top, 7)
+                .padding(.bottom, 7)
                 .padding(.leading, 10)
                 .padding(.trailing, 5)
                 .frame(width: 300)
-                .background(RoundedRectangle(cornerRadius: 5).fill(Color.white))
+                .background(RoundedRectangle(cornerRadius: 5).fill(Color.white.opacity(0.9)))
                 .padding(.bottom,5)
-            SecureField("Senha", text: $password)
-                .foregroundStyle(.black)
-                .padding(.top, 5)
-                .padding(.bottom, 5)
+            SecureField("", text: $password, prompt: Text("Senha").foregroundColor(Color(CGColor(red: 58 / 255, green: 166 / 255, blue: 200 / 255, alpha: 1))))
+                .foregroundStyle(Color(CGColor(red: 58 / 255, green: 166 / 255, blue: 200 / 255, alpha: 1)))
+                .padding(.top, 7)
+                .padding(.bottom, 7)
                 .padding(.leading, 10)
                 .padding(.trailing, 5)
                 .frame(width: 300)
-                .background(RoundedRectangle(cornerRadius: 5).fill(Color.white))
+                .background(RoundedRectangle(cornerRadius: 5).fill(Color.white.opacity(0.9)))
         }
     }
 }
