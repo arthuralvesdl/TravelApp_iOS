b837cc9ad8a340891fa21f92145aa73ad49b5382
arthuralvesdl <arthuralveslacerda@gmail.com>
2024-06-20 14:47:17 -0300

    feat: navigation

--- TravelApp/Views/LoginView.swift
+++ TravelApp/Views/LoginView.swift
@@ -3,11 +3,18 @@ import SwiftUI
 struct LoginView: View {
     @State private var user: String = ""
     @State private var password: String = ""
+    @State private var shouldLogin: Bool = false
+    @State private var shouldRegister: Bool = false
+    
     
     var body: some View {
         NavigationStack {
             ZStack {
-                Color(CGColor(red: 25 / 255, green: 130 / 255, blue: 100 / 255, alpha: 1)).ignoresSafeArea()
+                Image("backgroundImage")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .opacity(1)
                 
                 VStack (){
                     VStack(alignment: .center) {
@@ -18,61 +25,72 @@ struct LoginView: View {
                             .multilineTextAlignment(.center)
                             .foregroundStyle(.white)
                     }
+                    .padding(.top, 15)
                     .frame(maxWidth: .infinity)
                     Spacer()
                     
                     LoginInputsView(user: $user, password: $password)
                     
-                    Spacer()
-                    VStack (spacing: 10) {
-                        NavigationLink(destination: HomeView()){
-                            Text("Entrar")
-                                .foregroundColor(.black)
-                        }
-                        .frame(width: 110, height: 30)
-                        .background(.white)
-                        .cornerRadius(5)
-                        
-                        NavigationLink(destination: Text("Register")){
+                    HStack (spacing: 10) {
+                        Button(action: {
+                            shouldRegister = true
+                        }) {
                             Text("Registrar")
+                                .frame(width: 145, height: 30)
+                                .background(Color.white).ignoresSafeArea()
                                 .foregroundColor(.black)
+                                .cornerRadius(5)
                         }
-                        .frame(width: 110, height: 30)
-                        .background(.white)
-                        .cornerRadius(5)
+                        
+                        Button(action: {
+                            //validacao de login
+                            shouldLogin = true
+                        }) {
+                            Text("Entrar")
+                                .frame(width: 145, height: 30)
+                                .background(Color.white).ignoresSafeArea()
+                                .foregroundColor(.black)
+                                .cornerRadius(5)
+                        }
+                 
                     }.padding(.top, 10)
+                    Spacer()
                 }
             }
-        }.navigationTitle("Travel")
+            .navigationDestination(isPresented: $shouldLogin) {
+                HomeView()
+            }
+            .navigationDestination(isPresented: $shouldRegister) {
+                //tela de cadastro
+                Text("Register")
+            }
+        }
+        .navigationTitle("Travel")
     }
 }
 
 struct LoginInputsView: View {
     @Binding var user: String
-    @Binding  var password: String
+    @Binding var password: String
     var body: some View {
         VStack () {
             TextField("Usuário", text: $user)
-                .foregroundStyle(.white)
+                .foregroundStyle(.black)
                 .padding(.top, 5)
                 .padding(.bottom, 5)
                 .padding(.leading, 10)
                 .padding(.trailing, 5)
                 .frame(width: 300)
-                .background(RoundedRectangle(cornerRadius: 5).fill(Color.white.opacity(0.3)))
+                .background(RoundedRectangle(cornerRadius: 5).fill(Color.white))
                 .padding(.bottom,5)
             SecureField("Senha", text: $password)
-                .foregroundStyle(.white)
+                .foregroundStyle(.black)
                 .padding(.top, 5)
                 .padding(.bottom, 5)
                 .padding(.leading, 10)
                 .padding(.trailing, 5)
                 .frame(width: 300)
-                .background(RoundedRectangle(cornerRadius: 5).fill(Color.white.opacity(0.3)))
+                .background(RoundedRectangle(cornerRadius: 5).fill(Color.white))
         }
     }
 }
-
-//#Preview {
-//    LoginView()
-//}
