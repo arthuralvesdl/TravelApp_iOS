66736944107c69f6ea314a2de17bbd30a498393b
arthuralvesdl <arthuralveslacerda@gmail.com>
2024-07-01 14:39:51 -0300

    Create ProfileView.swift

---
+++ TravelApp/Views/ProfileView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ProfileView: View {
+    @ObservedObject var loginModel: LoginModel
+    var body: some View {
+        
+        ZStack{
+            
+        }
+    }
+}
+
